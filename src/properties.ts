c2bd95bada5668b3697fe05d3543b7beb20c5d1a
sya-ri <sya79lua@gmail.com>
2020-12-02 22:02:28 +0900

    Propertiesを名前空間に変更

--- src/properties.ts
+++ src/properties.ts
@@ -1,13 +1,15 @@
-const Properties = PropertiesService.getScriptProperties().getProperties();
+export namespace Properties {
+    const Properties = PropertiesService.getScriptProperties().getProperties();
 
-/**
- * SAVE_DRIVE_FOLDER_ID: ファイルを保存する先
- * - https://drive.google.com/drive/folders/SAVE_DRIVE_FOLDER_ID
- */
-const SaveDriveFolderID = Properties.SAVE_DRIVE_FOLDER_ID
+    /**
+     * SAVE_DRIVE_FOLDER_ID: ファイルを保存する先
+     * - https://drive.google.com/drive/folders/SAVE_DRIVE_FOLDER_ID
+     */
+    export const SaveDriveFolderID = Properties.SAVE_DRIVE_FOLDER_ID
 
-/**
- * SLACK_API_TOKEN: Slack Bot の API トークン
- * - Bot User OAuth Access Token
- */
-const SlackApiToken = Properties.SLACK_API_TOKEN
+    /**
+     * SLACK_API_TOKEN: Slack Bot の API トークン
+     * - Bot User OAuth Access Token
+     */
+    export const SlackApiToken = Properties.SLACK_API_TOKEN
+}
--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,12 @@
-import "./properties"
 import "./@types/slack"
+import { Properties} from "./properties"
 import { GoogleSpreadSheet } from "./data/spread_sheet"
 
 function testGetDriveFiles() {
-    GoogleSpreadSheet.updateFiles(SaveDriveFolderID)
+    GoogleSpreadSheet.updateFiles(Properties.SaveDriveFolderID)
     GoogleSpreadSheet.printFiles()
 }
 
 function testAuthSlackBot() {
-    Logger.log(SlackApp.create(SlackApiToken).authTest())
+    Logger.log(SlackApp.create(Properties.SlackApiToken).authTest())
 }
